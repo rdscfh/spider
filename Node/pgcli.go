f57f25d14f4db49240b25526fe7a989409420b4a
heyuzhen <yuzhen.he@3dxt.com>
2021-12-03 17:46:59 +0800

    改为sqlite

--- Node/pgcli.go
+++ Node/pgcli.go
@@ -4,14 +4,14 @@ import (
 	"log"
 
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 var db *gorm.DB
 var err error
 
 func init() {
-	db, err = gorm.Open("postgres", "host=localhost user=postgres dbname=gorm sslmode=disable password=68957423")
+	db, err = gorm.Open("sqlite3", "test.db")
 	if err != nil {
 		log.Panic(err)
 	}
